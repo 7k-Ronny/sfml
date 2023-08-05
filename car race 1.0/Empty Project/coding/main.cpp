0a7e2fbdc5af3a6015a1e975f27c054e719157aa
kshitizHancy <kshitizpaudel5@gmail.com>
2023-08-05 10:01:33 +0545

    third change

--- car race 1.0/Empty Project/coding/main.cpp
+++ car race 1.0/Empty Project/coding/main.cpp
@@ -4,6 +4,9 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "kshitiz");
     sf::CircleShape shape(200.f);
+    sf::RectangleShape rectangle(sf::Vector2f(120.f, 50.f));
+
+    rectangle.setFillColor(sf::Color::White);
     shape.setFillColor(sf::Color::Yellow);
 
     while (window.isOpen())
@@ -16,7 +19,9 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        // window.draw(shape);
+        window.draw(rectangle);
+        
         window.display();
     }
 
